0f49e72d1c3ce012b0adcb84111e80cb44642528
Kenny Baas-Schwegler <kenny@baasie.com>
2020-10-17 09:08:31 +0200

    lab 1 results

--- CinemaReservations/CSharp/CinemaAllocations.Domain/MovieScreening.cs
+++ CinemaReservations/CSharp/CinemaAllocations.Domain/MovieScreening.cs
@@ -12,5 +12,21 @@ namespace CinemaAllocations.Domain
         {
             _rows = rows;
         }
+
+        public SeatsAllocated AllocateSeats(AllocateSeats allocateSeats)
+        {
+            foreach (var row in _rows.Values)
+            {
+                var seatsAllocated = row.AllocateSeats(allocateSeats);
+                if (seatsAllocated.GetType() != typeof(NoPossibleAllocationsFound))
+                {
+                    var updatedRow = row.MakeSeatsReserved(seatsAllocated.ReservedSeats);
+                    _rows[updatedRow.Name] = updatedRow;
+                    return seatsAllocated;
+                }
+            }
+
+            return new NoPossibleAllocationsFound(allocateSeats.PartyRequested);
+        }
     }
 }
---
+++ CinemaReservations/CSharp/CinemaAllocations.Domain/TooManyTicketsRequested.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace CinemaAllocations.Domain
+{
+    public class TooManyTicketsRequested : SeatsAllocated
+    {
+        public TooManyTicketsRequested(int partyRequested) : base(new List<Seat>(), partyRequested)
+        {
+        }
+    }
+}
