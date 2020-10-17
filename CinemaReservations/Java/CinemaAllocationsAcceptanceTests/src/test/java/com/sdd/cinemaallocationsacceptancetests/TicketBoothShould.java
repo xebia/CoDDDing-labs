7395b515c93ad7c2803567c88f256b361caa5cf7
Kenny Baas-Schwegler <kenny@baasie.com>
2020-10-17 09:16:15 +0200

    lab 2 begin tests

--- CinemaReservations/Java/CinemaAllocationsAcceptanceTests/src/test/java/com/sdd/cinemaallocationsacceptancetests/TicketBoothShould.java
+++ CinemaReservations/Java/CinemaAllocationsAcceptanceTests/src/test/java/com/sdd/cinemaallocationsacceptancetests/TicketBoothShould.java
@@ -11,10 +11,11 @@ import static com.google.common.truth.Truth.assertThat;
 public class TicketBoothShould {
 
     private static final String FORD_THEATER = "1";
+    private static final String O3_AUDITORIUM = "2";
     private static final String DOCK_STREET = "3";
     private static final String MADISON_THEATER = "5";
 
-    MovieScreeningRepository repository = new StubMovieScreeningRepository();
+    private MovieScreeningRepository repository = new StubMovieScreeningRepository();
 
     public TicketBoothShould() throws IOException {
     }
@@ -38,9 +39,7 @@ public class TicketBoothShould {
         SeatsAllocated seatsAllocated = ticketBooth.allocateSeats(new AllocateSeats(DOCK_STREET, partyRequested));
 
         assertThat(seatsAllocated.reservedSeats()).hasSize(3);
-        assertThat(seatsAllocated.reservedSeats().get(0).toString()).isEqualTo("A6");
-        assertThat(seatsAllocated.reservedSeats().get(1).toString()).isEqualTo("A7");
-        assertThat(seatsAllocated.reservedSeats().get(2).toString()).isEqualTo("A8");
+        assertThat((seatsAllocated.seatNames())).containsExactly("A6", "A7", "A8");
     }
 
     @Test
@@ -61,6 +60,27 @@ public class TicketBoothShould {
         SeatsAllocated seatsAllocated = ticketBooth.allocateSeats(new AllocateSeats(MADISON_THEATER, partyRequested));
 
         assertThat(seatsAllocated).isInstanceOf(TooManyTicketsRequested.class);
+    }
+
+    @Test
+    public void reserve_three_adjacent_seats_when_available() {
+        int partyRequested = 3;
+
+        TicketBooth ticketBooth = new TicketBooth(repository);
+        SeatsAllocated seatsAllocated = ticketBooth.allocateSeats(new AllocateSeats(O3_AUDITORIUM, partyRequested));
+
+        assertThat(seatsAllocated.reservedSeats()).hasSize(3);
+        assertThat((seatsAllocated.seatNames())).containsExactly("A8", "A9", "A10");
+    }
+
+    @Test
+    public void return_NoPossibleAdjacentSeatsFound_when_4_tickets_are_requested() {
+        int partyRequested = 4;
+
+        TicketBooth ticketBooth = new TicketBooth(repository);
+        SeatsAllocated seatsAllocated = ticketBooth.allocateSeats(new AllocateSeats(O3_AUDITORIUM, partyRequested));
+
+        throw new UnsupportedOperationException("NOT IMPLEMENTED");
     }
 
 }
--- CinemaReservations/Java/CinemaAllocationsDomain/src/main/java/com/sdd/cinemaallocations/SeatsAllocated.java
+++ CinemaReservations/Java/CinemaAllocationsDomain/src/main/java/com/sdd/cinemaallocations/SeatsAllocated.java
@@ -3,11 +3,17 @@ package com.sdd.cinemaallocations;
 import lombok.Value;
 import lombok.experimental.NonFinal;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Value
 @NonFinal
 public class SeatsAllocated {
     int partyRequested;
     List<Seat> reservedSeats;
+
+    public List<String> seatNames() {
+        return reservedSeats.stream().sorted(Comparator.comparing(Seat::number)).map(Seat::toString).collect(Collectors.toList());
+    }
 }
