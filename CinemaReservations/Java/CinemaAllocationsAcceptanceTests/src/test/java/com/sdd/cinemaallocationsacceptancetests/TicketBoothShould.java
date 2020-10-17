7f3b5bf6408c89fe972115d467a90e4add6ceeb6
Kenny Baas-Schwegler <kenny@baasie.com>
2020-10-17 09:08:31 +0200

    lab 1 results

--- CinemaReservations/Java/CinemaAllocationsAcceptanceTests/src/test/java/com/sdd/cinemaallocationsacceptancetests/TicketBoothShould.java
+++ CinemaReservations/Java/CinemaAllocationsAcceptanceTests/src/test/java/com/sdd/cinemaallocationsacceptancetests/TicketBoothShould.java
@@ -1,9 +1,6 @@
 package com.sdd.cinemaallocationsacceptancetests;
 
-import com.sdd.cinemaallocations.AllocateSeats;
-import com.sdd.cinemaallocations.MovieScreeningRepository;
-import com.sdd.cinemaallocations.SeatsAllocated;
-import com.sdd.cinemaallocations.TicketBooth;
+import com.sdd.cinemaallocations.*;
 import com.sdd.cinemaallocationsacceptancetests.helpers.StubMovieScreeningRepository;
 import org.junit.Test;
 
@@ -17,7 +14,7 @@ public class TicketBoothShould {
     private static final String DOCK_STREET = "3";
     private static final String MADISON_THEATER = "5";
 
-    private final MovieScreeningRepository repository = new StubMovieScreeningRepository();
+    MovieScreeningRepository repository = new StubMovieScreeningRepository();
 
     public TicketBoothShould() throws IOException {
     }
@@ -34,13 +31,36 @@ public class TicketBoothShould {
     }
 
     @Test
-    public void return_SeatsNotAvailable_when_all_seats_are_unavailable() throws IOException {
-        throw new UnsupportedOperationException();
+    public void Reserve_multiple_seats_when_available() {
+        int partyRequested = 3;
+
+        TicketBooth ticketBooth = new TicketBooth(repository);
+        SeatsAllocated seatsAllocated = ticketBooth.allocateSeats(new AllocateSeats(DOCK_STREET, partyRequested));
+
+        assertThat(seatsAllocated.reservedSeats()).hasSize(3);
+        assertThat(seatsAllocated.reservedSeats().get(0).toString()).isEqualTo("A6");
+        assertThat(seatsAllocated.reservedSeats().get(1).toString()).isEqualTo("A7");
+        assertThat(seatsAllocated.reservedSeats().get(2).toString()).isEqualTo("A8");
     }
 
     @Test
-    public void return_TooManyTicketsRequested_when_9_tickets_are_requested() throws IOException {
-        throw new UnsupportedOperationException();
+    public void return_SeatsNotAvailable_when_all_seats_are_unavailable() {
+        int partyRequested = 1;
+
+        TicketBooth ticketBooth = new TicketBooth(repository);
+        SeatsAllocated seatsAllocated = ticketBooth.allocateSeats(new AllocateSeats(MADISON_THEATER, partyRequested));
+
+        assertThat(seatsAllocated).isInstanceOf(NoPossibleAllocationsFound.class);
+    }
+
+    @Test
+    public void return_TooManyTicketsRequested_when_9_tickets_are_requested() {
+        int partyRequested = 9;
+
+        TicketBooth ticketBooth = new TicketBooth(repository);
+        SeatsAllocated seatsAllocated = ticketBooth.allocateSeats(new AllocateSeats(MADISON_THEATER, partyRequested));
+
+        assertThat(seatsAllocated).isInstanceOf(TooManyTicketsRequested.class);
     }
 
 }
--- CinemaReservations/Java/CinemaAllocationsDomain/src/main/java/com/sdd/cinemaallocations/Row.java
+++ CinemaReservations/Java/CinemaAllocationsDomain/src/main/java/com/sdd/cinemaallocations/Row.java
@@ -3,6 +3,7 @@ package com.sdd.cinemaallocations;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @EqualsAndHashCode
@@ -14,5 +15,30 @@ public class Row {
     public Row(String name, List<Seat> seats) {
         this.name = name;
         this.seats = seats;
+    }
+
+    public SeatsAllocated allocateSeats(AllocateSeats allocateSeats) {
+        SeatAllocation allocation = new SeatAllocation(allocateSeats.partyRequested());
+
+        for (Seat seat : this.seats()) {
+
+            if (seat.isAvailable())
+            {
+                allocation.addSeat(seat);
+
+                if(allocation.isFulfilled()) {
+                    return new SeatsAllocated(allocateSeats.partyRequested(), allocation.allocatedSeats());
+                }
+            }
+        }
+        return new NoPossibleAllocationsFound(allocateSeats.partyRequested(), new ArrayList<>());
+    }
+
+    public Row makeSeatsReserved(List<Seat> updatedSeats) {
+
+        for (Seat newSeat : updatedSeats) {
+            this.seats.set(seats.indexOf(newSeat), newSeat.reserveSeats());
+        }
+        return new Row(this.name, this.seats);
     }
 }
